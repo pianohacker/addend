94007b0c9b80dc4ee319caf606462753f31b44e8
Jesse Weaver <pianohacker@gmail.com>
2017-09-25 23:24:04 -0600

    Add new hold/save mechanism, implement else

--- source/builtins.fs
+++ source/builtins.fs
@@ -4,7 +4,7 @@
 : literal immediate
 	' lit ,
 	,
-	;
+;
 
 \ ## Character constants
 : '\n' 10 ;
@@ -14,17 +14,36 @@
 
 \ # Control structures
 : if immediate
-	hold
+	record
 	' 0branch ,
+	\ Save location of offset on stack
+	here @ >h
 	0 ,
+;
+
+: else immediate
+	\ Fetch offset address
+	h>
+
+	\ Shove `branch` to the end of the `else` block on the hold stack.
+	' branch ,
+	here @ >h
+	0 ,
+
+	\ Now, fill in the position just past that branch in the original zbranch offset.
+	dup
+	here @ swap -
+	!
 ;
 
 : endif immediate
 	h@
-	here @ h@ - 
+	here @ h> -
 	!
+
 	play
 ;
 
 \ # I/O
-
+0 if 33 emit else 65 emit endif
+1 if 66 emit else 34 emit endif
